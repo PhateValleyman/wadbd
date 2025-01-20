7c4eed7fcfbc096e39b1f4a883875e214c91c146
rhythmcache <153998419+rhythmcache@users.noreply.github.com>
2025-01-20 19:50:16 +0530

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,30 +1,22 @@
 ui_print "[*] Checking installation environment..."
 if [ "$APATCH" ]; then
-    ui_print " => APatch: $APATCH_VER │ $APATCH_VER_CODE"
-    ap_dir="/data/adb/ap/bin"
-    if [ -d "$ap_dir" ]; then
-        ln -sf "$MODPATH/system/bin/wadbd" "$ap_dir/wadbd"
-        ui_print " => Created symlink"
-    else
-        ui_print " => APatch directory not found. Skipping symlink."
-    fi
-    rm -f "$MODPATH/action.sh"
+    ui_print "- APatch: $APATCH_VER │ $APATCH_VER_CODE"
+    bin_dir="/data/adb/ap/bin"
 elif [ "$KSU" ]; then
-    ui_print " => KSU: $KSU_KERNEL_VER_CODE │ $KSU_VER_CODE"
-    ks_dir="/data/adb/ksu/bin"
-    if [ -d "$ks_dir" ]; then
-        ln -sf "$MODPATH/system/bin/wadbd" "$ks_dir/wadbd"
-        ui_print " => Created symlink"
-    else
-        ui_print " => KernelSU directory not found. Skipping symlink."
-    fi
-    rm -f "$MODPATH/action.sh"
+    ui_print "- KSU: $KSU_KERNEL_VER_CODE │ $KSU_VER_CODE"
+    bin_dir="/data/adb/ksu/bin"
 elif [ "$MAGISK_VER_CODE" ]; then
-    ui_print " => Magisk: $MAGISK_VER │ $MAGISK_VER_CODE"
+    ui_print "- Magisk: $MAGISK_VER │ $MAGISK_VER_CODE"
 else
-    ui_print "! Recovery is not supported."
-    abort ""
+    ui_print " ! Recovery is not supported."
+    abort
 fi
+if [ "$bin_dir" ] && [ -d "$bin_dir" ]; then
+    cp "$MODPATH/system/bin/wadbd" "$bin_dir/wadbd"
+    chmod 755 "$bin_dir/wadbd"
+    touch "$MODPATH/skip_mount"
+fi
+
 ui_print "[*] Checking USB debugging status..."
 if getprop persist.sys.usb.config | grep -q "adb"; then
     ui_print " => USB debugging is enabled."
@@ -35,13 +27,9 @@ fi
 
 ui_print ""
 ui_print " => 🔍 Gathering device information..."
-sleep 1
 ui_print "📱 => Manufacturer: $(getprop ro.product.manufacturer)"
 ui_print "📱 => Model: $(getprop ro.product.model)"
 ui_print "📱 => Android Version: $(getprop ro.build.version.release)"
 ui_print "📱 => Security Patch: $(getprop ro.build.version.security_patch)"
-ui_print "🔧 => Build Fingerprint: $(getprop ro.build.fingerprint)"
-sleep 1
-
 set_perm_recursive "$MODPATH/system" 0 0 0755 0755
 ui_print "=> Installation Completed!"
